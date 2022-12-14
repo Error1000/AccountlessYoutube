9e9743fd98d6514242e78bf1aedf6084794c5b5d
Udrea Dan <udreadanmihai@gmail.com>
2022-12-14 22:30:32 +0200

    Sort feed by latest videos

--- main.js
+++ main.js
@@ -1,20 +1,61 @@
 class Video {
-    constructor(channel, href, thumbnailImageURL, title, durationSecs, views, ageString) {
+    constructor(channel, href, thumbnailImageURL, title, durationSecs, views, ageSecs) {
         this.channel = channel;
         this.href = href;
         this.thumbnailImageURL = thumbnailImageURL;
         this.title = title;
         this.durationSecs = durationSecs;
         this.views = views;
-        this.ageString = ageString;
+        this.ageSecs = ageSecs;
     }
 }
 
-var subscriptions = ['jacksepticeye'];
+var subscriptions = new Set([]);
 var feedVideos = [];
 var lastUpdated = new Date(0);
 
 console.log("Accountless-youtube content script loaded!");
+
+// NOTE: Will scrape youtube to get the data
+function addSubscriptionVideos(subscription) {
+    subscription = subscription.toString().trim();
+    console.log("Adding videos of sub: ", subscription);
+    var channelUrl = "https://www.youtube.com/@" + subscription + "/videos";
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var parser = new DOMParser();
+            var documentToScrape = parser.parseFromString(request.responseText, "text/html");
+            // Extract variable from scripts
+            var jsonSring = Array.from(documentToScrape.getElementsByTagName("script"))
+                .map(script => script.innerText.match(/ytInitialData *= *([^;]*);/)).filter(res => res !== null)[0][1]; /* select first regex match result, then select the second match of that regex match result to get the result of the capture group */
+            var data = JSON.parse(jsonSring)["contents"]["twoColumnBrowseResultsRenderer"]["tabs"]
+                .find(tab => tab["tabRenderer"]["selected"])["tabRenderer"]["content"]["richGridRenderer"]["contents"];
+            for (var video of data) {
+                var video = video["richItemRenderer"]["content"]["videoRenderer"];
+                var thumbnailUrl = video["thumbnail"]["thumbnails"].slice(-1)[0]["url"];
+                var id = video["navigationEndpoint"]["watchEndpoint"]["videoId"];
+                var title = video["title"]["runs"].map(run => run["text"]).join();
+                var duration = fromHHMMSS(video["lengthText"]["simpleText"]);
+                var views = parseInt(video["viewCountText"]["simpleText"].replace(/\D/g, ''));
+                var age = fromHumanAge(video["publishedTimeText"]["simpleText"]);
+                feedVideos.push(new Video(subscription, "/watch?v=" + id, thumbnailUrl, title, duration, views, age));
+            }
+
+        }
+    }
+
+    request.open("GET", channelUrl, false/* NOT async */);
+    request.send();
+    // Make sure we keep feed sorted
+    feedVideos = feedVideos.sort((a, b) => { if (a.ageSecs < b.ageSecs) { return -1; } else if (a.ageSecs > b.ageSecs) { return 1; } else { return 0; } });
+}
+
+function removeSubscriptionVideos(subscription) {
+    feedVideos = feedVideos.filter((video) => video.channel !== subscription);
+}
+
+
 
 function localStorageExists() {
     try {
@@ -27,19 +68,21 @@ function localStorageExists() {
     }
 }
 
-// NOTE: This function will initialize the values to defaults if it couldn't read them and will report success
 async function readStorageData() {
     if (!localStorageExists()) return false;
-    feedVideos = (await browser.storage.local.get("cachedVideos"))["cachedVideos"];
-    if (feedVideos === undefined || feedVideos === null) {
-        feedVideos = [];
+    var subscriptionsRead = (await browser.storage.local.get("subscriptions"))["subscriptions"];
+    if (subscriptionsRead !== undefined && subscriptionsRead !== null) {
+        subscriptions = new Set(subscriptionsRead);
     }
 
-    lastUpdated = (await browser.storage.local.get("lastUpdated"))["lastUpdated"];
-    if (lastUpdated === undefined || lastUpdated === null) {
-        lastUpdated = new Date(0);
-    } else {
-        lastUpdated = Date.parse(lastUpdated);
+    var feedVideosRead = (await browser.storage.local.get("cachedVideos"))["cachedVideos"];
+    if (feedVideosRead !== undefined && feedVideosRead !== null) {
+        feedVideos = feedVideosRead;
+    }
+
+    var lastUpdatedRead = (await browser.storage.local.get("lastUpdated"))["lastUpdated"];
+    if (lastUpdatedRead !== undefined && lastUpdatedRead !== null) {
+        lastUpdated = new Date(Date.parse(lastUpdatedRead));
     }
 
     return true;
@@ -47,6 +90,7 @@ async function readStorageData() {
 
 async function writeStorageData() {
     if (!localStorageExists()) return false;
+    await browser.storage.local.set({ "subscriptions": Array.from(subscriptions) });
     await browser.storage.local.set({ "lastUpdated": lastUpdated.toJSON() });
     await browser.storage.local.set({ "cachedVideos": feedVideos });
     return true;
@@ -59,44 +103,97 @@ window.addEventListener("pageshow", async () => {
         console.log("Updating feed!");
         feedVideos = [];
         for (var subscription of subscriptions) {
-            var channelUrl = "https://www.youtube.com/@" + subscription + "/videos";
-            let request = new XMLHttpRequest();
-            request.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    var parser = new DOMParser();
-                    var documentToScrape = parser.parseFromString(request.responseText, "text/html");
-                    // Extract variable from scripts
-                    var jsonSring = Array.from(documentToScrape.getElementsByTagName("script"))
-                        .map(script => script.innerText.match(/ytInitialData *= *([^;]*);/)).filter(res => res !== null)[0][1]; /* select first regex match result, then select the second match of that regex match result to get the result of the capture group */
-                    var data = JSON.parse(jsonSring)["contents"]["twoColumnBrowseResultsRenderer"]["tabs"]
-                        .find(tab => tab["tabRenderer"]["selected"])["tabRenderer"]["content"]["richGridRenderer"]["contents"];
-                    for (var video of data) {
-                        var video = video["richItemRenderer"]["content"]["videoRenderer"];
-                        var thumbnailUrl = video["thumbnail"]["thumbnails"].slice(-1)[0]["url"];
-                        var id = video["navigationEndpoint"]["watchEndpoint"]["videoId"];
-                        var title = video["title"]["runs"].map(run => run["text"]).join();
-                        var duration = fromHHMMSS(video["lengthText"]["simpleText"]);
-                        var views = parseInt(video["viewCountText"]["simpleText"].replace(/\D/g, ''));
-                        var age = video["publishedTimeText"]["simpleText"];
-                        feedVideos.push(new Video(subscription, "/watch?v=" + id, thumbnailUrl, title, duration, views, age));
-                    }
-
-                }
-            }
-
-            request.open("GET", channelUrl, false/* NOT async */);
-            request.send();
+            addSubscriptionVideos(subscription);
         }
         lastUpdated = new Date();
         await ensure(writeStorageData);
     }
-
     await ensure(hijackFeed);
 }, false);
 
 
 
 // Used to format video data to show
+
+
+// Takes human dates likes: 10 years, or 2.5 minutes
+// And returns: that date in seconds
+function fromHumanAge(string) {
+    const unitMap = {
+        "second": 1,
+        "seconds": 1,
+        "minute": 60,
+        "minutes": 60,
+        "hour": 60 * 60,
+        "hours": 60 * 60,
+        "day": 24 * 60 * 60,
+        "days": 24 * 60 * 60,
+        "week": 7 * 24 * 60 * 60,
+        "weeks": 7 * 24 * 60 * 60,
+        "month": 30 * 24 * 60 * 60,
+        "months": 30 * 24 * 60 * 60,
+        "year": 12 * 30 * 24 * 60 * 60,
+        "years": 12 * 30 * 24 * 60 * 60,
+        "decade": 10 * 12 * 30 * 24 * 60 * 60,
+        "decades": 10 * 12 * 30 * 24 * 60 * 60,
+    };
+
+    let splits = /(\d*.?\d+) +([^ ]+)/.exec(string).splice(1);
+    return parseFloat(splits[0]) * unitMap[splits[1]];
+}
+
+function toHumanAge(secs) {
+    var decades = Math.round((secs / (10 * 12 * 30 * 24 * 60 * 60)) % (10 * 12 * 30 * 24 * 60 * 60) * 10) / 10;
+    if (decades > 1) {
+        return decades + " decades";
+    } else if (decades == 1) {
+        return decades + " decade";
+    }
+
+    var years = Math.round(((secs / (12 * 30 * 24 * 60 * 60)) % (12 * 30 * 24 * 60 * 60)) * 10) / 10;
+    if (years > 1) {
+        return years + " years";
+    } else if (years == 1) {
+        return years + " year";
+    }
+
+    var months = Math.round(((secs / (30 * 24 * 60 * 60)) % (30 * 24 * 60 * 60)) * 10) / 10;
+    if (months > 1) {
+        return months + " months";
+    } else if (months == 1) {
+        return months + " month";
+    }
+
+    var days = Math.round(((secs / (24 * 60 * 60)) % (24 * 60 * 60)) * 10) / 10;
+    if (days > 1) {
+        return days + " days";
+    } else if (days == 1) {
+        return days + " day";
+    }
+
+    var hours = Math.round(((secs / (60 * 60)) % (60 * 60)) * 10) / 10;
+    if (hours > 1) {
+        return hours + " hours";
+    } else if (hours == 1) {
+        return hours + " hour";
+    }
+
+    var minutes = Math.round(((secs / 60) % 60) * 10) / 10;
+    if (minutes > 1) {
+        return minutes + " minutes";
+    } else if (minutes == 1) {
+        return minutes + " minute";
+    }
+
+    var seconds = Math.round((secs % 60) * 10) / 10;
+    if (seconds > 1) {
+        return seconds + " seconds";
+    } else if (seconds == 1) {
+        return seconds + " second";
+    } else {
+        return seconds + " seconds";
+    }
+}
 
 function toHHMMSS(secs) {
     var hours = Math.floor(secs / (60 * 60));
@@ -146,17 +243,15 @@ const feedCSSToInject = `
     div.injected-yt-feed {
         margin-top: 10px;
         margin-left: 10px;
-        width: 100%;
-        height: 100%;
     }
 
     div.injected-yt-video {
         float: left;
         width: 250px;
-        aspect-ratio: 1.1;
+        height: 25.5em;
+        overflow: hidden;
         margin-left: 8px;
         margin-right: 8px;
-        margin-bottom: 6px;
     }
 
     div.injected-yt-video > a {
@@ -299,7 +394,8 @@ function hijackFeed() {
             injected_feed.classList.add("injected-yt-feed");
             content.appendChild(injected_feed);
             for (const video of feedVideos) {
-                injectVideo(injected_feed, video.href, "/@" + video.channel, video.thumbnailImageURL, toHHMMSS(video.durationSecs), video.title, video.channel, formatViews(video.views) + " views • " + video.ageString);
+                var sinceLastUpdateSecs = (new Date() - lastUpdated) / 1000;
+                injectVideo(injected_feed, video.href, "/@" + video.channel, video.thumbnailImageURL, toHHMMSS(video.durationSecs), video.title, video.channel, formatViews(video.views) + " views • " + toHumanAge(video.ageSecs + sinceLastUpdateSecs) + " ago");
             }
 
             return true;
@@ -308,11 +404,55 @@ function hijackFeed() {
     return false;
 }
 
+
+function tryGetChannel() {
+    var handle = document.querySelector("yt-formatted-string[id=\"channel-handle\"]");
+    if (handle !== null) return handle.innerText;
+    var videoChannelName = document.querySelector("yt-formatted-string.ytd-channel-name");
+    if (videoChannelName !== null) return videoChannelName.getElementsByTagName("a")[0].href.split('/').slice(-1)[0];
+    return null;
+}
+
 function hijackSubscribeButton() {
-    var theButton = document.querySelector("yt-button-shape.ytd-subscribe-button-renderer");
-    if (theButton !== null) {
-        console.log(theButton);
-        return true;
+    let theBell = document.querySelector("div#notification-preference-button");
+    if (theBell !== null) {
+        theBell.remove();
+        var theButton = document.querySelector("yt-button-shape.ytd-subscribe-button-renderer");
+        if (theButton !== null) {
+            var theButton = theButton.getElementsByTagName("button")[0];
+            var buttonTextElement = theButton.querySelector("span[role=\"text\"]");
+            var theChannel = tryGetChannel().slice(1);
+            if (theChannel === null) return false;
+
+            if (subscriptions.has(theChannel)) {
+                buttonTextElement.innerText = "Unsubscribe";
+            } else {
+                buttonTextElement.innerText = "Subscribe"
+            }
+            theButton.onclick = async () => {
+                // Make sure we are in sync
+                await ensure(readStorageData);
+                if (buttonTextElement.innerText === "Subscribe") {
+                    try {
+                        subscriptions.add(theChannel);
+                        addSubscriptionVideos(theChannel);
+                    } catch (e) {
+                        // FIXME
+                    }
+                    buttonTextElement.innerText = "Unsubscribe";
+                } else {
+                    try {
+                        subscriptions.delete(theChannel);
+                        removeSubscriptionVideos(theChannel);
+                    } catch (e) {
+                        // FIXME
+                    }
+                    buttonTextElement.innerText = "Subscribe"
+                }
+                ensure(writeStorageData);
+            };
+            return true;
+        }
     }
     return false;
 }
@@ -355,3 +495,5 @@ async function ensure(f, waitingFunction = () => { return sleep(100); }) {
 
 // TODO: Make sure using waitForDomChange is actually better
 ensure(readStorageData).then(ensure(hijackFeed, waitForDomChange));
+ensure(() => { hijackSubscribeButton(); return false; }, waitForDomChange);
+
